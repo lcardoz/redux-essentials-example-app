a4c998167b90514fd429bea486fd255f1175226b
Leah Cardoz <leahcardoz@gmail.com>
2023-06-21 11:37:22 -0400

    added postAdded reducer function and exported action creator in postsSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -13,10 +13,22 @@ const initialState = [
   }
 ]
 
+// don't try to mutate any data outside of createSlice!
+// createSlice() converts mutations into safe immutable updates internally using the Immer library
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    // when we write the postAdded reducer function, createSlice automagically 
+    // generates an "action creator" function with the same name:
+    postAdded(state, action) {
+      state.push(action.payload)
+    }
+  }
 })
+
+// we then export the action creator and use it in our components to 
+// dispatch the action when the user clicks "save post":
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer;
