59256019e44eed71b5b6561b32998f545b1430d3
Leah Cardoz <leahcardoz@gmail.com>
2023-06-21 11:23:36 -0400

    created AddPostForm component to create a post and imported into App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import { Navbar } from './app/Navbar';
 import { PostsList } from './features/posts/PostsList';
+import { AddPostForm } from './features/posts/AddPostForm.js';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             path="/"
             render={() => (
               <>
+                <AddPostForm />
                 <PostsList />
               </>
             )}
