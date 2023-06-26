1fc4291f4a7ad6fde6d320c8e1d486b527ce8893
Leah Cardoz <leahcardoz@gmail.com>
2023-06-26 11:55:40 -0400

    added logic to store dates for posts with TimeAgo component, sorted the Posts list by newest posts, added post reaction buttons and logic to count likes

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
+import { ReactionButtons } from './ReactionButtons';
 
 export const SinglePostPage = ({ match }) => {
 
@@ -28,9 +30,11 @@ export const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.title}</h2>
         <div>
-          <PostAuthor userId={post.user}/>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
         </div>
         <p className="post-section">{post.content}</p>
+        <ReactionButtons post={post} />
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,17 +1,22 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 const initialState = [
   {
     id: '1', 
     title: 'First Post!',
     content: 'Hello!',
-    user: '0'
+    user: '0',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
   },
   {
     id: '2',
     title: 'Second Post',
     content: 'More text',
-    user: '2'
+    user: '2',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
   },
 ]
 
@@ -29,11 +34,20 @@ const postsSlice = createSlice({
         return {
           payload: {
             id: nanoid(),
+            date: new Date().toISOString(),
             title,
             content,
             user: userId,
-          }
+            reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0},
+          },
         }
+      }
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
       }
     },
     postUpdated(state, action) {
@@ -49,6 +63,6 @@ const postsSlice = createSlice({
 
 // we then export the action creator and use it in our components to 
 // dispatch the action when the user clicks "save post" or "edit post":
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer;
