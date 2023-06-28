713cfe2a12768512d9c09ee165c78c31e9b93852
Leah Cardoz <leahcardoz@gmail.com>
2023-06-28 12:21:34 -0400

    created async thunks to fetch and load API posts, extracted post selectors, loaded state for requests, added extra reducers, displayed loading state

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export const Spinner = ({ text = '', size = '5em' }) => {
   const header = text ? <h4>{text}</h4> : null
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,24 +1,33 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
+import { client } from '../../api/client';
 
-const initialState = [
-  {
-    id: '1', 
-    title: 'First Post!',
-    content: 'Hello!',
-    user: '0',
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-  },
-  {
-    id: '2',
-    title: 'Second Post',
-    content: 'More text',
-    user: '2',
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-  },
-]
+// using API to fetch initial state data now: We'll switch our state from being an array of posts by itself, to look like {posts, status, error}. We'll also remove the old sample post entries from our initial state. 
+// prev hardcoded initial state:
+// const initialState = [
+//   {
+//     id: '1', 
+//     title: 'First Post!',
+//     content: 'Hello!',
+//     user: '0',
+//     date: sub(new Date(), { minutes: 10 }).toISOString(),
+//     reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+//   },
+//   {
+//     id: '2',
+//     title: 'Second Post',
+//     content: 'More text',
+//     user: '2',
+//     date: sub(new Date(), { minutes: 5 }).toISOString(),
+//     reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+//   },
+// ]
+
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
 
 // don't try to mutate any data outside of createSlice!
 // createSlice() converts mutations into safe immutable updates internally using the Immer library
@@ -28,7 +37,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer (state, action) {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
       prepare(title, content, userId) {
         return {
@@ -45,19 +54,33 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const existingPost = state.posts.find(post => post.id === postId)
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.find(post => post.id === id)
+      const existingPost = state.posts.find(post => post.id === id)
       if (existingPost) {
         existingPost.title = title
         existingPost.content = content
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
   }
 })
 
@@ -66,3 +89,19 @@ const postsSlice = createSlice({
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer;
+
+// like any abstraction, it's not something you should do all the time, everywhere. 
+// Writing selectors means more code to understand and maintain. 
+// Don't feel like you need to write selectors for every single field of your state. 
+// Try starting without any selectors, and add some later when you find yourself 
+// looking up the same values in many parts of your application code.
+export const selectAllPosts = state => state.posts.posts
+
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  // response object looks like { data:[] }
+  return response.data
+  // response.data returns just the array of posts
+})
